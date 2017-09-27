85510d1e042a9f5182b496e25e553b8b7b14cf40
Melissa Boyer <melissaboyer3000@gmail.com>
2017-09-27 18:11:02 -0400

    Initial Commit

---
+++ server.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+@app.route("/")
+def index():
+    """Return home page."""
+
+    return render_template("index.html")
+
+
+# @app.route("/error")
+# def error():
+#     raise Exception("Error!")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
+
+    # PORT = int(os.environ.get("PORT", 5000))
+    # DEBUG = "NO_DEBUG" not in os.environ
+
+    # app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
